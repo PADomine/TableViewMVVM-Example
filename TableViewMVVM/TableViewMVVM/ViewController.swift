5bc4cb89e0cf147a78cbcebb3503400a1f970372
Pablo <padomine@gmail.com>
2017-05-31 18:36:05 -0300

    Base project

---
+++ TableViewMVVM/TableViewMVVM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableViewMVVM
+//
+//  Created by Pablo Dominé on 5/31/17.
+//  Copyright © 2017 Pablo Dominé. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
